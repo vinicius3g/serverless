3d264814de2a7280513ee15de8b8d6852d6e8abf
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-26 16:41:10 -0300

    feat: handler com toda logica do lambda

--- handler.js
+++ handler.js
@@ -1,56 +1,125 @@
-// import { Client } from 'pg'
-// const {Client} = require('pg')
+import AWS from "aws-sdk";
+import express from "express";
+import serverless from "serverless-http";
+import { v4 as uuidv4 } from "uuid";
 
-import pkg from 'pg';
-const { Client } = pkg;
+const app = express();
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
-const client = new Client({
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
+app.use(express.json());
 
-export const handler = async (event) => {
-  // TODO implement
-
-  //   const createTableQuery = `
-  //   CREATE TABLE IF NOT EXISTS funcionarios (
-  //     id SERIAL PRIMARY KEY,
-  //     nome VARCHAR(100) NOT NULL,
-  //     idade INT NOT NULL,
-  //     cargo VARCHAR(100) NOT NULL
-  //   );
-  // `;
-
-  // const createTable = async () => {
-  //   try {
-  //     await client.connect();
-  //     await client.query(createTableQuery);
-  //     console.log("Tabela 'funcionarios' criada com sucesso.");
-  //   } catch (err) {
-  //     console.error('Erro ao criar a tabela:', err);
-  //   } finally {
-  //     await client.end();
-  //   }
-  // };
-
-  // createTable();
+app.get("/users/:userId", async (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
 
   try {
-    await client.connect();
-    const result = await client.query('SELECT NOW()');
-    console.log(result.rows[0].now, 'RODOU');
+    const { Item } = await dynamoDbClient.get(params).promise();
+
+    if (Item) {
+      const { userId, name, age, position_name } = Item;
+
+      return res.json({
+        userId,
+        name,
+        age,
+        position_name
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ error: 'Could not find user with provided "userId"' });
+    }
   } catch (error) {
-    console.error('Error executing query:', error.stack);
-  } finally {
-    await client.end();
+    console.log(error);
+    return res.status(500).json({ error: "Could not retreive user" });
   }
+});
 
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify('Hello from Lambda!'),
-  // };
+app.post("/users", async (req, res) => {
+  const { name, age, position_name } = req.body;
+  const userId = uuidv4();
 
-  // return response;
-};
+  const params = {
+    TableName: USERS_TABLE,
+    Item: {
+      userId,
+      name,
+      age,
+      position_name
+    },
+  };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+    return res.json({
+      userId,
+      name,
+      age,
+      position_name
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Could not create user" });
+  }
+});
+
+app.put("/users/:userId", async (req, res) => {
+  const { name, age, position_name } = req.body;
+
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+    UpdateExpression: "set #name = :name, #age = :age, #position_name = :position_name",
+    ExpressionAttributeNames: {
+      "#name": "name",
+      "#age": "age",
+      "#position_name": "position_name"
+    },
+    ExpressionAttributeValues: {
+      ":name": name,
+      ":age": age,
+      ":position_name": position_name
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  try {
+    const { Attributes } = await dynamoDbClient.update(params).promise();
+    return res.json(Attributes);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Could not update user" });
+  }
+});
+
+app.delete("/users/:userId", async (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId,
+    },
+  };
+
+  try {
+    await dynamoDbClient.delete(params).promise();
+    return res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Could not delete user" });
+  }
+});
+
+app.use((_, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export const handler = serverless(app);
